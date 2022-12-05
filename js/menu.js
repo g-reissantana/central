01e9686e577ab4a44ea349973a9df5b015ad3617
Gabriel <gabriel.familia32@hotmail.com>
2022-12-04 23:14:10 -0300

    feat: balls animate

--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,62 @@
+
+// Toggle menu mobile
 const menuIcon = document.querySelector('[data-menu="icon"]')
 const menuMobile = document.querySelector('[data-menu="mobile"]')
 let menuOpened = false
 
-// Open/Close menu mobile
-menuIcon.addEventListener('click', toggleMenu)
+function closeMenu(menu) {
+  menu.style.height = '0vh'
+}
 
-function toggleMenu() {
+function openMenu(menu) {
 
-  let menu = menuMobile.style
-  
-  menu.transition = 'all 600ms'
+  menu.style.height = '100vh'
+
+
+  menuMobile.querySelectorAll('a').forEach(el => {
+    el.addEventListener('click', () => {
+      closeMenu(menuMobile)
+      menuOpened = false
+    })
+  })
+}
+
+function toggleMenu(menu) {
+
+  menu.style.transition = 'all 600ms'
 
   if (menuOpened) {
-    menu.height = '0vh'
+    closeMenu(menu)
     menuOpened = false
   } else {
-    menu.height = '100vh'
+    openMenu(menu)
     menuOpened = true
   }
+
 }
+
+menuIcon.addEventListener('click', () => toggleMenu(menuMobile))
+
+// Sliders
+
+const sliders = document.querySelector('[data-slider="sliders"]')
+const slideWidth = sliders.firstElementChild.offsetWidth
+let c = 0
+
+sliders.addEventListener('touch', () => alert('ola'))
+
+setInterval(() => {
+
+  let teste = sliders.style
+  teste.transition = 'all 200ms'
+  teste.marginLeft = `-${c}px`
+  c += slideWidth + 16
+
+  if(c > (slideWidth+16) * 7) {
+    c = 0
+  }
+
+  console.log(slideWidth)
+  console.log(c)
+
+}, 2000)
